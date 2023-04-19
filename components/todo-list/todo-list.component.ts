3080a52ffe02df094e408176085ec542394c41e3
Karolina <karo129@wp.pl>
2023-04-19 20:38:15 +0200

    chagned input editing styles in css(todo-item component)

--- components/todo-list/todo-list.component.ts
+++ components/todo-list/todo-list.component.ts
@@ -1,53 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { newTodo } from 'src/app/newTodo.interface';
-import { TodoListService } from 'src/app/services/todo-list.service';
+import { Component, OnInit } from '@angular/core'
+import { newTodo } from 'src/app/newTodo.interface'
+import { TodoListService } from 'src/app/services/todo-list.service'
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+	selector: 'app-todo-list',
+	templateUrl: './todo-list.component.html',
+	styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
+	newTodo: string = ''
+	todos: newTodo[] = []
+	listVisible: boolean = false
 
-  newTodo: any;
-  todos: newTodo[] = [];
-  currentStatus = '';
-  listVisible: boolean = false;
+	constructor(private todolistservice: TodoListService) {}
 
-  constructor(private todolistservice: TodoListService) {}
+	ngOnInit() {
+		this.todos = this.todolistservice.getTasks()
+	}
 
-  ngOnInit() {
-    this.todos = this.todolistservice.getTasks();
-  }
+	addTodo() {
+		const todo: newTodo = {
+			title: this.newTodo,
+			done: false,
+			editing: false,
+		}
+		this.todolistservice.addTask(todo)
+		this.newTodo = ''
+		this.listVisible = true
+	}
 
-  addTodo() {
+	toggleTodo(todo: any) {
+		this.todolistservice.toggleTodo(todo)
+	}
 
-    const todo: newTodo = {
-      title: this.newTodo,
-      done: false,
-      editing: false,
-    };
-    this.todolistservice.addTask(todo);
-    this.newTodo = '';
-    this.listVisible = true;
-  }
+	toggleAll() {
+		this.todolistservice.toggleAll()
+	}
 
-  toggleTodo(todo: any) {
-    this.todolistservice.toggleTodo(todo);
-  }
-
-  toggleAll() {
-    this.todolistservice.toggleAll();
-  }
-
-  clearCompleted() {
-    this.todos = this.todos.filter((todo) => !todo.done);
-  }
-
-  getTasks() {
-    
-  }
-
-
-
+	clearCompleted() {
+		this.todos = this.todos.filter(todo => !todo.done)
+	}
 }
