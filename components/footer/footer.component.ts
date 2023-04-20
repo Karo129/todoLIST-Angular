84dba9e5e09f8ba251e339a1f9423d81d7534d75
Karolina <karo129@wp.pl>
2023-04-20 11:27:44 +0200

    added footer component

--- components/footer/footer.component.ts
+++ components/footer/footer.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { newTodo } from 'src/app/newTodo.interface';
+import { TodoListService } from 'src/app/services/todo-list.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  todos: newTodo[] = [];
 
-  constructor() { }
+  constructor(private todolistservice: TodoListService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.todos = this.todolistservice.getTasks();
+  }
+ 
+  counting() {
+    return this.todos.filter((todo) => !todo.done).length;
   }
 
 }
